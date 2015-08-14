c68aa17ccac56c65fd1eb46d3711888c961af10a
Ken Rinfeld B. Garcia <kenrinfeld.cybertechcebu@gmail.com>
2015-08-14 15:55:07 +0800

    Css configure userProfile and banner Image

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813215106) do
+ActiveRecord::Schema.define(version: 20150814074209) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "body",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "title",       limit: 255
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "user_id",     limit: 4
   end
 
   create_table "users", force: :cascade do |t|
